09fc9c83e56151c0f7b11e95b406c6a1134751b0
HuongNguyenQuoc <quochuong05ln@gmail.com>
2026-07-28 13:12:09 +0700

    config again for port backend

--- Frontend/vite.config.ts
+++ Frontend/vite.config.ts
@@ -1,20 +1,20 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
-import path from 'node:path'
+import tailwindcss from "@tailwindcss/vite";
+import react from "@vitejs/plugin-react";
+import path from "node:path";
+import { defineConfig } from "vite";
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   resolve: {
-    alias: { '@': path.resolve(__dirname, './src') },
+    alias: { "@": path.resolve(__dirname, "./src") },
   },
   server: {
     port: 5173,
     proxy: {
-      '/api': {
-        target: 'http://localhost:3000',
+      "/api": {
+        target: "http://localhost:5000",
         changeOrigin: true,
       },
     },
   },
-})
+});
